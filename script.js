2bf610929f1bfdb87a600cbc6e1a3b3a3062384e
philine <philine@hotz.com>
2023-11-21 16:01:30 +0100

    Finished exercise01

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-export function exercise01(args) {
+export function exercise01(args) /*defining the function*/ {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if (currentElement === "e" || currentElement === "E") {
+      result.push ("")
+    }
+    else {
+       result.push(currentElement)
+    } 
+   
   }
-  return result.join("")
+  return result.join("") 
+  
 }
+

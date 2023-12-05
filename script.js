40f0bbce50728272656066512188522d56e930a4
philine <philine@hotz.com>
2023-12-05 15:36:43 +0100

    finished exercise 02

--- script.js
+++ script.js
@@ -10,9 +10,25 @@ export function exercise01(args) /*defining the function*/ {
     else {
        result.push(currentElement)
     } 
-   
   }
   return result.join("") 
-  
+
 }
 
+export function exercise02(args) {
+  return args.toUpperCase()
+}
+
+
+
+
+
+
+
+
+/*between the curly brackets defines whats the function does */
+/*result = endproduct*/
+/*computer starting counting at zero*/
+/*i++ shortcut for decrease by one*/
+/*return result.join("") --> add or delete pieces between letters*/
+/* not equals --> currentElement != "e" &&  */
